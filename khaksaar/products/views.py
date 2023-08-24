a7da3b0547d12eca67cf852bcfa29f876469d0ea
HamdaRaheen <140147600+HamdaRaheen@users.noreply.github.com>
2023-08-24 04:41:45 -0700

    djangorepo

--- khaksaar/products/views.py
+++ khaksaar/products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render  # Import your sample product data from the products module
 from .models import Product
 def product_list(request):
+    products = Product.objects.all()
     context = {
         'products': Product,  # Use your sample product data from the products module
     }
     return render(request, 'products.html', context)
 
 def productdetails(request, productIndex):
+    products = Product.objects.all()
     if productIndex >= 0:
         
         return render(request, 'productdetails.html')
