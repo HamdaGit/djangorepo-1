c7a9220a0e0d3adb5fd1219b0693c69b368dba78
HamdaRaheen <140147600+HamdaRaheen@users.noreply.github.com>
2023-09-13 14:34:05 -0700

    djangorepo

--- khaksaar/cart/urls.py
+++ khaksaar/cart/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('add_to_cart/<int:productIndex>/', views.add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<int:productIndex>/', views.remove_from_cart, name='remove_from_cart'),
     path('cart/', views.cart, name='cart'),
-     path("checkout/", views.checkout, name="checkout"),     
+    path("checkout/", views.checkout, name="checkout"),  
+    path('order/', views.order, name='order'),   
 ]
     
     # Other URLs
