e77c6f600800e7be30bef2248b5932140ca76d6a
HamdaRaheen <140147600+HamdaRaheen@users.noreply.github.com>
2023-09-15 05:25:35 -0700

    djangorepo

--- khaksaar/khaksaar/views.py
+++ khaksaar/khaksaar/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from products.models import Product
+from cart.models import CartItem, Cart
 
-def index(request, productIndex=None):  # Provide a default value of None
-    if productIndex is not None:
-        product = get_object_or_404(Product, pk=productIndex)
-    else:
-        product = None  # Handle the case where productIndex is None
-    context = {
-        'product': product,
-        'productIndex': productIndex,
-    }
-    return render(request, 'index.html', context)
+def index(request):  # Provide a default value of None
+    # In your view
+        cart = Cart.objects.get_cart(request)
+
+        cart_items = CartItem.objects.filter(cart=cart)  # Replace some_cart with your actual cart
+        context = {
+            'cart_items': cart_items,
+}
+        return render(request, 'index.html', context)
+
+  
